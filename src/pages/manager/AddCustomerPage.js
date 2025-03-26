5e027cc8151e0584cc0fb819ea84df23d69ba4bf
Marianna Matola <mutnenka.mk@gmail.com>
2025-03-26 11:39:26 +0200

    Solution

--- src/pages/manager/AddCustomerPage.js
+++ src/pages/manager/AddCustomerPage.js
@@ -3,9 +3,35 @@ const { expect } = require('@playwright/test');
 export class AddCustomerPage {
   constructor(page) {
     this.page = page; 
+    this.firstNameField = page.getByPlaceholder('First Name');
+    this.lastNameField = page.getByPlaceholder('Last Name');
+    this.postCodeField = page.getByPlaceholder('Post Code');
+    this.addCustomerButton = page.getByRole('form').getByRole('button', { name: 'Add Customer' });
+    this.customersButton = page.getByRole('button', { name: 'Customers' });
   }
+
 
   async open() {
     await this.page.goto('/angularJs-protractor/BankingProject/#/manager/addCust');
+  }
+
+  async fillFirstNameField(firstName) {
+    await this.firstNameField.fill(firstName);
+  }
+
+  async fillLastNameField(lastName) {
+    await this.lastNameField.fill(lastName);
+  }
+
+  async fillPostCodeField(postCode) {
+    await this.postCodeField.fill(postCode);
+  }
+  
+  async clickAddCustomerButton() {
+    await this.addCustomerButton.click();
+  }
+
+  async clickCustomersButton() {
+    await this.customersButton.click();
   }
 }
--- src/pages/manager/CustomersListPage.js
+++ src/pages/manager/CustomersListPage.js
@@ -3,9 +3,35 @@ const { expect } = require('@playwright/test');
 export class CustomersListPage {
   constructor(page) {
     this.page = page; 
+    this.lastAddedCustomerRow = page.getByRole('row').last();
+    this.lastAddedCustomerFirstName = this.lastAddedCustomerRow.getByRole('cell').nth(0);
+    this.lastAddedCustomerLastName = this.lastAddedCustomerRow.getByRole('cell').nth(1);
+    this.lastAddedCustomerPostCode = this.lastAddedCustomerRow.getByRole('cell').nth(2);
+    this.lastAddedCustomerAccountNumber = this.lastAddedCustomerRow.getByRole('cell').nth(3);
   }
 
   async open() {
     await this.page.goto('/angularJs-protractor/BankingProject/#/manager/list');
   }
+
+  async waitForPageLoad() {
+    await this.page.waitForURL('/angularJs-protractor/BankingProject/#/manager/list')
+  }
+
+  async assertLastAddedCustomerFirstName(firstName) {
+    await expect(this.lastAddedCustomerFirstName).toContainText(firstName);
+  }
+
+  async assertLastAddedCustomerLastName(lastName) {
+    await expect(this.lastAddedCustomerLastName).toContainText(lastName);
+  }
+
+  async assertLastAddedCustomerPostCode(postCode) {
+    await expect(this.lastAddedCustomerPostCode).toContainText(postCode);
+  }
+
+  async assertLastAddedCustomerAccountNumberIsAbsent() {
+    await expect(this.lastAddedCustomerAccountNumber).toContainText('');
+  }
+
 }
